60889689a0fead030ed0f6f219a20d47a64dec04
Dynamo15 <131327726+Dynamo15@users.noreply.github.com>
2026-04-08 22:52:34 -0600

    se añadio efecto de pareticulas que se pueden desplazar

--- src/componentes/ThreeScene.jsx
+++ src/componentes/ThreeScene.jsx
@@ -12,29 +12,38 @@ export default function ThreeScene() {
     const width = window.innerWidth;
     const height = window.innerHeight;
     const loader = new THREE.TextureLoader();
+    
 
     // ESCENA
     const scene = new THREE.Scene();
 
     // CÁMARA
-    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    camera.position.z = 5;
+    const camera = new THREE.PerspectiveCamera(40, width / height, 0.1, 1000);
+    camera.position.x = 4;  //mover la camara en eje x
+    camera.position.z = 10.5;  //mover la camara en eje z
+    camera.position.y = -1;    // MOVER LA CAMARA EN EJE Y 
+    
 
     // RENDER
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(width, height);
-    renderer.setPixelRatio(window.devicePixelRatio, 1.5); // ✅ AQUÍ
+    renderer.setPixelRatio(window.devicePixelRatio, 1.5); 
     mountRef.current.appendChild(renderer.domElement);
 
     let animationId;
     let lastTime = 0;
-    
-      loader.load(
-      "/textures/earth.jpg",
-      
+    let dissolve = 0;
 
-      (texture) => {
-        console.log("imagen cargada ✅"); 
+    window.addEventListener("scroll", () => {
+          const scrollY = window.scrollY;
+          const maxScroll = window.innerHeight;
+
+          dissolve = Math.min(scrollY / maxScroll, 1);
+        });
+    
+      loader.load("/textures/earth.jpg",(texture) => {
+      
+        console.log("imagen cargada"); 
         const canvas = document.createElement("canvas");
         const ctx = canvas.getContext("2d");
         const particleTexture = new THREE.TextureLoader().load("/textures/particle.png");
@@ -45,6 +54,8 @@ export default function ThreeScene() {
         canvas.height = img.height;
 
         ctx.drawImage(img, 0, 0);
+
+        
 
         const imageData = ctx.getImageData(0, 0, img.width, img.height).data;
 
@@ -72,7 +83,6 @@ export default function ThreeScene() {
             }
           }
         }
-        
 
         console.log("vertices:", vertices.length);
 
@@ -85,7 +95,7 @@ export default function ThreeScene() {
 
         //const step = 8;
 
-        const material = new THREE.PointsMaterial({
+        const material = new THREE.PointsMaterial({            //materiales de puntos
             size: 0.03,
             map: particleTexture,
             transparent: true,
@@ -101,8 +111,57 @@ export default function ThreeScene() {
 
         
 
-        const animate = (time) => {
+
+        //TARGET
+                                        //x  y   z
+        const target = new THREE.Vector3(4, 6, 2);
+        const lineY = 3;     // altura (navbar)
+        const lineWidth = 6; // ancho de la línea
+
+        const targetMesh = new THREE.Mesh(               
+          new THREE.SphereGeometry(0.05),
+          new THREE.MeshBasicMaterial({ color: 0xff0000 })
+        );
+
+        targetMesh.position.copy(target);
+        scene.add(targetMesh);             
+        //
+
+
+
+        //parte donde ocurre la animacion
+
+        const animate = (time) => {                              
           animationId = requestAnimationFrame(animate);
+
+          
+          //dissolve effect
+          const positions = geometry.attributes.position.array;
+
+          for (let i = 0; i < positions.length; i += 3) {
+            const factor = Math.random();
+
+            if (factor < dissolve) {
+              const px = positions[i];
+              const py = positions[i + 1];
+              const pz = positions[i + 2];
+
+              // 🔥 destino en una línea horizontal
+              const targetX = (Math.random() - 0.5) * lineWidth; // dispersión horizontal
+              const targetY = lineY; // altura fija (navbar)
+              const targetZ = 0;
+
+              const dirX = targetX - px;
+              const dirY = targetY - py;
+              const dirZ = targetZ - pz;
+
+              positions[i] += dirX * 0.02;
+              positions[i + 1] += dirY * 0.02;
+              positions[i + 2] += dirZ * 0.02;
+            }
+          }
+          geometry.attributes.position.needsUpdate = true;
+          ////////////////////////////////////////////////////////////////////
 
           if (time - lastTime < 16) return;
 
@@ -110,6 +169,7 @@ export default function ThreeScene() {
           lastTime = time;
 
           points.rotation.y += 0.003;
+          points.position.x = 0; 
           renderer.render(scene, camera);
         };
 
@@ -119,7 +179,7 @@ export default function ThreeScene() {
       undefined,
 
       (error) => {
-        console.error("error cargando imagen ❌", error); 
+        console.error("error cargando imagen ", error); 
       }
     );
 
